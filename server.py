bfb6935febd12d09fc96eec170b9ec8c066ec99f
jesuscalderondev <jesuscalderonunimag@gmail.com>
2024-03-23 02:58:33 -0500

    generator qr success

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, redirect, jsonify, flash
+from flask import Flask, render_template, redirect, jsonify, flash, send_file, session
 from os import getenv
+import random
+import qrcode
 
 app = Flask("QR UNIMAG")
 
@@ -14,6 +16,28 @@ app.config['MAIL_PASSWORD'] = getenv("MAIL_PASSWORD")
 def index():
     return render_template("index.html")
 
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/generate/qr")
+def generateQr():
+    num = random.randint(0, 9999999)
+    text = f"prueba{num}" 
+    qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=7, border=2)
+    qr.add_data(text)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    img.save("static/img/qr.png")
+    return redirect("/home")
+
+@app.route("/download/qr")
+def downloadQr():
+    file = "static/img/qr.png"
+    return send_file(file, as_attachment=True)
 
 if __name__ == "__main__":
     app.run(debug=True)
