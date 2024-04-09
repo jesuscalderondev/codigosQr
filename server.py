0ce78c49d367d155ab7769978b9caa2761e7c2dc
jesuscalderondev <jesuscalderondev>
2024-04-08 21:22:01 -0500

    despliegue

--- server.py
+++ server.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template, redirect, jsonify, flash, send_file, session, request
+from flask import Flask, render_template, redirect, jsonify, flash, send_file, request
 from os import getenv
 from database import *
 from datetime import timedelta, datetime
 from flask_mail import Mail
 from mailManager import sendMessage
 from dotenv import load_dotenv
+from flask import session as cloud
 
 from pdfGeneratorCh import Generator
 
 app = Flask("QR UNIMAG")
+
+app.config['SECRET_KEY']= getenv('SECRET_KEY')
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
@@ -18,47 +21,68 @@ app.config['MAIL_USERNAME'] = getenv('USER_MAIL')
 app.config['MAIL_PASSWORD'] = getenv('MAIL_PASSWORD')
 mail = Mail()
 
+def validateSession():
+    return 'valid' in cloud
+
 @app.route("/")
 def index():
+    if validateSession():
+        return redirect("/home")
     return render_template("index.html")
 
 
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    if validateSession():
+        return render_template("home.html")
+    return redirect("/")
 
-@app.route("/download/qr")
-def downloadQr():
-    file = "static/img/qr.png"
-    return send_file(file, as_attachment=True)
 
 @app.route("/sendQr", methods=["POST"])
 def generar():
-    try:
-        data = request.get_json()
-        Generator(int(data["tickets"]))
-        #sendMessage(mail, data['email'], data['fullname'])
-        return jsonify(response = "success", message = "Código(s) envíados al destinatario")
-    except Exception as e:
-        print(e)
-        return jsonify(respose = "failed", message = "No lo envía")
+    if validateSession():
+        try:
+            data = request.get_json()
+            Generator(int(data["tickets"]))
+            #sendMessage(mail, data['email'], data['fullname'])
+            return jsonify(response = "success", message = "Código(s) envíados al destinatario")
+        except Exception as e:
+            print(e)
+            return jsonify(respose = "failed", message = "No lo envía")
+    return jsonify(response = "failed", message = "Sin acceso al sistema")
 
-@app.route("/verify/<string:qr>")
+@app.route("/scan/<string:qr>")
 def verifyQr(qr):
-    code = session.get(Codigo, qr)
 
-    if code != None and code.usado == False:
-        code.usado = True
-        session.add(code)
-        session.commit()
+    if validateSession():
+        code = session.get(Codigo, qr)
 
-        return f"Boleto #{code.boleto} válido"
+        if code != None and code.usado == False:
+            code.usado = True
+            session.add(code)
+            session.commit()
+
+            return f"Boleto #{code.boleto} válido"
     return "Este boleto no es válido para el ingreso"
+
+@app.route('/login', methods=['POST'])
+def login():
+
+    if request.method == 'POST':
+        data = request.form
+
+        if data['email'] == 'syste+@gmail.com' and data['password'] == 'syste+2024':
+            cloud['valid'] = True
+            return redirect("/home")
+    return redirect('/')
 
 
 @app.route("/copy/database")
 def copyDatabase():
-    return send_file("database.db")
+    if validateSession():
+        return send_file("database.db")
+    else:
+        return "Nooo"
 
         
 if __name__ == "__main__":
